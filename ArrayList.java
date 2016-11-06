a8a33272b1180b7b3b8ffdcbbc03af200a24e547
aworto01 <aworto01@mail.bbk.ac.uk>
2016-11-06 11:10:38 +0000

    Implemented ArrayList get, size, isEmpty and constructor

---
+++ ArrayList.java
@@ -0,0 +1,97 @@
+public class ArrayList implements List{
+
+    private int size = 0;
+    private Object[] arr;
+
+    public ArrayList(int startingSize = 10){
+        this.arr = new Object[startingSize];
+    }
+    
+	public boolean isEmpty(){
+        return this.size == 0;
+    }
+
+	public int size(){
+        return this.size;
+    }
+
+	/**
+	 * Returns the element at the given position. 
+	 * 
+	 * If the index is negative or greater or equal than the size of
+	 * the list, then an appropriate error must be returned.
+	 * 
+	 * @param index the position in the list of the item to be retrieved
+	 * @return the element or an appropriate error message, 
+	 *         encapsulated in a ReturnObject
+	 */
+	public ReturnObject get(int index){
+        ErrorMessage error = ErrorMessage.NO_ERROR;
+        Object returnObject = null;
+
+        if(index == 0){
+            error = ErrorMessage.EMPTY_STRUCTURE;
+        } 
+        else if(index >= this.size || index < 0){
+            error = ErrorMessage.INDEX_OUT_OF_BOUNDS;
+        }
+        else{
+            returnObject = arr[index];
+        }
+
+        return new ReturnObjectImpl(returnObject, error);        
+    }
+
+	/**
+	 * Returns the elements at the given position and removes it
+	 * from the list. The indeces of elements after the removed
+	 * element must be updated accordignly.
+	 * 
+	 * If the index is negative or greater or equal than the size of
+	 * the list, then an appropriate error must be returned.
+	 * 
+	 * @param index the position in the list of the item to be retrieved
+	 * @return the element or an appropriate error message, 
+	 *         encapsulated in a ReturnObject
+	 */
+	public ReturnObject remove(int index){
+
+    }
+
+	/**
+	 * Adds an element to the list, inserting it at the given
+	 * position. The indeces of elements at and after that position
+	 * must be updated accordignly.
+	 * 
+	 * If the index is negative or greater or equal than the size of
+	 * the list, then an appropriate error must be returned.
+	 * 
+	 * If a null object is provided to insert in the list, the
+	 * request must be ignored and an appropriate error must be
+	 * returned.
+	 * 
+	 * @param index the position at which the item should be inserted in
+	 *              the list
+	 * @param item the value to insert into the list
+	 * @return an ReturnObject, empty if the operation is successful
+	 *         or containing an appropriate error message otherwise
+	 */
+	public ReturnObject add(int index, Object item){
+
+    }
+
+	/**
+	 * Adds an element at the end of the list.
+	 * 
+	 * If a null object is provided to insert in the list, the
+	 * request must be ignored and an appropriate error must be
+	 * returned.
+	 * 
+	 * @param item the value to insert into the list
+	 * @return an ReturnObject, empty if the operation is successful
+	 *         or containing an appropriate error message otherwise
+	 */
+	public ReturnObject add(Object item){
+
+    }
+}
