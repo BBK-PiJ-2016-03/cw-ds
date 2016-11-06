1e611f20b9bece718422c23c1b386164a57bc9f9
aworto01 <aworto01@mail.bbk.ac.uk>
2016-11-06 10:09:33 +0000

    Initial IMplementation of ReturnObjectImpl

---
+++ ReturnObjectImpl.java
@@ -0,0 +1,26 @@
+public class ReturnObjectImpl implements ReturnObject{
+
+    private ErrorMessage errorMessage;
+    private Object returnObject;
+
+    public ReturnObjectImpl(Object returnObject){
+        ReturnObjectImpl(returnObject, null);
+    }
+
+    public ReturnObjectImpl(Object returnObject, ErrorMessage errorMessage){        
+        this.returnObject = returnObject;
+        this.errorMessage = errorMessage == null ? ErrorMessage.NO_ERROR : errorMessage;        
+    }
+
+    public boolean hasError(){
+        return errorMessage != ErrorMessage.NO_ERROR;
+    }
+
+    public ErrorMessage getError(){
+        return errorMessage;
+    }
+
+    public Object getReturnValue(){
+        return errorMessage == ErrorMessage.NO_ERROR ? returnObject : null; 
+    }
+}
