e977dcb7e81a7b4cc2f7c9bc2b456e547bcc02bd
aworto01 <aworto01@mail.bbk.ac.uk>
2016-11-08 10:33:26 +0000

    Refactored unit tests

--- Tests.java
+++ Tests.java
@@ -7,142 +7,194 @@ public class Tests{
     }
 
     private void arrayListTests(){
-        ReturnObject ret = null;
-        System.out.println("----New ArrayList");
-        ArrayList list = new ArrayList();    
 
-        ret = list.get(-1);
-        assertEquals(true, ret.hasError(), "Element retrieved at index -1 with error");
-        assertEquals(ErrorMessage.EMPTY_STRUCTURE, ret.getError(), "Error is EMPTY_STRUCTURE");    
+        List list = new ArrayList();
 
+        boolean isEmpty = testIsEmpty(list);
+        boolean size = testSize(list);
+        boolean get = testGet(list);
+        boolean remove = testRemove(list);
+        boolean add = testAdd(list);
+        boolean insert = testInsert(list);
 
-        assertEquals(true, list.isEmpty(), "isEmpty at initialisation");
-        assertEquals(0, list.size(), "Size 0 at initialisation");
+        System.out.println(isEmpty
+                        && size
+                        && get
+                        && remove
+                        && add
+                        && insert
+                        ? "All tests passed"
+                        : "Test failure"
+                        );
 
-        
-        //System.out.println("size: " + list.size());
-        System.out.println("----Adding 1 element");
-        ret = list.add(1);
-        
-        assertEquals(false, list.isEmpty(), "isEmpty false once holding an element");
-        assertEquals(false, ret.hasError(), "Element added without error");
-        assertEquals(null, ret.getReturnValue(), "Returned object is null");
-
-        ret = list.get(-1);
-
-        assertEquals(true, ret.hasError(), "Element retrieved at index -1 with error");
-        assertEquals(ErrorMessage.INDEX_OUT_OF_BOUNDS, ret.getError(), "Error is INDEX_OUT_OF_BOUNDS"); 
-
-        ret = list.get(50);
-
-        assertEquals(true, ret.hasError(), "Element retrieved at index 50 with error");
-        assertEquals(ErrorMessage.INDEX_OUT_OF_BOUNDS, ret.getError(), "Error is INDEX_OUT_OF_BOUNDS"); 
-
-        ret = list.get(0);
-
-        assertEquals(false, ret.hasError(), "Element retrieved at index 0 without error");
-        assertEquals(1, ret.getReturnValue(), "Returned value matches input"); 
-
-        ret = list.add(-1, 1);
-        assertEquals(true, ret.hasError(), "Element added at index -1 with error");
-        assertEquals(ErrorMessage.INDEX_OUT_OF_BOUNDS, ret.getError(), "Error is out of bounds");
-
-        System.out.println("----Adding 4 more elements");
-        for(int i = 2; i <= 5; i++){
-            list.add(i);
-        }
-        System.out.println("Array size: " + list.getArraySize());    
-        System.out.println("----Adding 5 more elements"); 
-        for(int i = 6; i <= 10; i++){
-            list.add(i);
-        }
-        System.out.println("Array size: " + list.getArraySize());    
-        System.out.println("----Adding 5 more elements");
-        for(int i = 11; i <= 15; i++){
-            list.add(i);
-        }
-        System.out.println("Array size: " + list.getArraySize());    
-        System.out.println("----Adding 5 more elements");
-        for(int i = 16; i <= 20; i++){
-            list.add(i);
-        }
-
-        assertEquals(20, list.size(), "Size 20");
-
-        System.out.println("Array size: " + list.getArraySize()); 
-        System.out.println("----Adding 10,000 more elements");
-        for(int i = 1; i <= 10000; i++){
-            list.add(i);
-        }
-        System.out.println("Array size: " + list.getArraySize()); 
 
         
 
-        //System.out.println("Added 21 ints... ");
-        //System.out.println("size: " + list.size());
+        
 
-        //System.out.println(list.toString());
 
-        //System.out.println("----------");
-        //System.out.println("Removing 5 from the beginning... ");
-        ret = list.remove(0);
-        //if(ret.hasError()) System.out.println(ret.getError());
-        ret = list.remove(0);
-        //if(ret.hasError()) System.out.println(ret.getError());
-        ret = list.remove(0);
-        //if(ret.hasError()) System.out.println(ret.getError());
-        ret = list.remove(0);
-        //if(ret.hasError()) System.out.println(ret.getError());
-        ret = list.remove(0);
-        //if(ret.hasError()) System.out.println(ret.getError());
-        //System.out.println("size: " + list.size());
-        //System.out.println(list.toString());
-
-        //System.out.println("----------");
-        //System.out.println("Inserting 1-5 at index 5... ");
-        ret = list.add(1,5);
-        //if(ret.hasError()) System.out.println(ret.getError());
-        ret = list.add(2,5);
-        //if(ret.hasError()) System.out.println(ret.getError());
-        ret = list.add(3,5);
-        //if(ret.hasError()) System.out.println(ret.getError());
-        ret = list.add(4,5);
-        //if(ret.hasError()) System.out.println(ret.getError());
-        ret = list.add(5,5);
-        //if(ret.hasError()) System.out.println(ret.getError());
-        //System.out.println("size: " + list.size());
-        //System.out.println(list.toString());
-
-        int count = 0;
-        while(!list.isEmpty()){
-            list.remove(0);
-            count++;
-        }
-        assertEquals(true, list.isEmpty(), "isEmpty after removing all elements. "+count+" elements removed");
+        
 
         //max test 2 147 483 639
-        for(int i = 1; i <= Integer.MAX_VALUE - 10; i++){
-            list.add(1);
-            if(i % 100000 == 0)
-                System.out.println("iteration: " + i + " array size: " + list.getArraySize());
-        }
-        System.out.println("Array size: " + list.getArraySize());
+        // for(int i = 1; i <= Integer.MAX_VALUE - 10; i++){
+        //     list.add(1);
+        //     if(i % 100000 == 0)
+        //         System.out.println("iteration: " + i + " array size: " + list.getArraySize());
+        // }
+        // System.out.println("Array size: " + list.getArraySize());
     }
 
+    private boolean testRemove(List list){
+        ReturnObject ret = null;
+        populateList(list);
 
-    private void assertEquals(Object expected, Object attained, String test){
+        int size = list.size();
+        Object value = list.get(0).getReturnValue();
+
+        ret = list.remove(0);
+        boolean t1 = assertEquals(size-1, list.size(), "Size reduced by 1");
+        boolean t2 = assertEquals(false, ret.hasError(), "Element removed without error");
+        boolean t3 = assertEquals(ErrorMessage.NO_ERROR, ret.getError(), "Error is NO_ERROR");
+        boolean t4 = assertEquals(value, ret.getReturnValue(), "Returned value matches the value at index");
+
+        ret = list.remove(list.size());
+        boolean t5 = assertEquals(size, list.size(), "Size unchanged");
+        boolean t6 = assertEquals(true, ret.hasError(), "Element not removed with error");
+        boolean t7 = assertEquals(ErrorMessage.INDEX_OUT_OF_BOUNDS, ret.getError(), "Error is INDEX_OUT_OF_BOUNDS");
+
+        ret = list.remove(-10);
+        boolean t8 = assertEquals(size, list.size(), "Size unchanged");
+        boolean t9 = assertEquals(true, ret.hasError(), "Element not removed with error");
+        boolean t10 = assertEquals(ErrorMessage.INDEX_OUT_OF_BOUNDS, ret.getError(), "Error is INDEX_OUT_OF_BOUNDS");
+
+        clearList(list);
+
+        return t1 && t2 && t3 && t4 && t5 && t6 && t7 && t8 && t9 && t10;
+    }
+
+    private boolean testInsert(List list){
+        ReturnObject ret = null;
+
+        ret = list.add(-1, 1);
+        boolean t1 = assertEquals(true, ret.hasError(), "Element added at index -1 with error");
+        boolean t2 = assertEquals(ErrorMessage.INDEX_OUT_OF_BOUNDS, ret.getError(), "Error is out of bounds");
+        boolean t3 = assertEquals(0, list.size(), "list still has size 0");
+
+        populateList(list);
+
+        Object originalValue = list.get(5).getReturnValue();
+
+        ret = list.add(5, 500);
+
+        boolean t4 = assertEquals(false, ret.hasError(), "Element added at index 5 without error");
+        boolean t5 = assertEquals(ErrorMessage.NO_ERROR, ret.getError(), "NO_ERROR is returned");
+        boolean t6 = assertEquals(500, list.get(5).getReturnValue(), "Value inserted at index is returned from index");
+        boolean t7 = assertEquals(originalValue, list.get(6).getReturnValue(), "Value previously at index is returned from index + 1");
+        boolean t8 = assertEquals((int)originalValue-1, list.get(4).getReturnValue(), "Value at index is returned from index");
+        boolean t9 = assertEquals(101, list.size(), "list is sized 101");
+
+        clearList(list);
+
+        return t1 && t2 && t3 && t4 && t5 && t6 && t7 && t8 && t9;
+        
+    }
+
+    private boolean testAdd(List list){
+        ReturnObject ret = null;
+
+        ret = list.add(1);
+        
+        boolean t1 = assertEquals(false, ret.hasError(), "Element added without error");
+        boolean t2 = assertEquals(ErrorMessage.NO_ERROR, ret.getError(), "Returned error is NO_ERROR");
+        boolean t3 = assertEquals(null, ret.getReturnValue(), "Returned object is null");
+
+        ret = list.add(null);
+        
+        boolean t4 = assertEquals(true, ret.hasError(), "Null element returned error");
+        boolean t5 = assertEquals(ErrorMessage.INVALID_ARGUMENT, ret.getError(), "Null element returned INVALID_ARGUMENT");
+        boolean t6 = assertEquals(null, ret.getReturnValue(), "Returned object is null");
+
+        clearList(list);
+
+        return t1 && t2 && t3 && t4 && t5 && t6;
+    }
+
+    private boolean testIsEmpty(List list){
+        boolean t1 = assertEquals(true, list.isEmpty(), "isEmpty at initialisation");
+        populateList(list);
+        boolean t2 = assertEquals(false, list.isEmpty(), "isEmpty false when filled");
+        clearList(list);
+        boolean t3 = assertEquals(true, list.isEmpty(), "isEmpty at initialisation");
+        return t1 && t2 && t3;
+    }
+
+    private boolean testSize(List list){
+        boolean t1 = assertEquals(0, list.size(), "Size 0");
+        populateList(list);
+        boolean t2 = assertEquals(100, list.size(), "Size 100");
+        clearList(list);
+        boolean t3 = assertEquals(0, list.size(), "Size 0");  
+        return t1 && t2 && t3;  
+    }
+
+    private boolean testGet(List list){
+        ReturnObject ret = null;
+        System.out.println("----New ArrayList");            
+
+        ret = list.get(-1);
+        boolean t1 = assertEquals(true, ret.hasError(), "Element retrieved at index -1 with error");
+        boolean t2 = assertEquals(ErrorMessage.EMPTY_STRUCTURE, ret.getError(), "Error is EMPTY_STRUCTURE");
+        boolean t3 = assertEquals(null, ret.getReturnValue(), "Returned object is null");
+
+        populateList(list);
+
+        ret = list.get(0);
+        boolean t4 = assertEquals(false, ret.hasError(), "Element retrieved at index 0 out error");
+        boolean t5 = assertEquals(ErrorMessage.NO_ERROR, ret.getError(), "Error is NO_ERROR");
+        boolean t6 = assertEquals(0, ret.getReturnValue(), "Returned value is 0");
+
+        ret = list.get(99);
+        boolean t7 = assertEquals(false, ret.hasError(), "Element retrieved at index 0 out error");
+        boolean t8 = assertEquals(ErrorMessage.NO_ERROR, ret.getError(), "Error is NO_ERROR");
+        boolean t9 = assertEquals(99, ret.getReturnValue(), "Returned value is 99");
+
+        ret = list.get(100);
+        boolean t10 = assertEquals(true, ret.hasError(), "Element retrieved at index -1 with error");
+        boolean t11 = assertEquals(ErrorMessage.EMPTY_STRUCTURE, ret.getError(), "Error is EMPTY_STRUCTURE");
+        boolean t12 = assertEquals(null, ret.getReturnValue(), "Returned object is null");
+
+        clearList(list);
+ 
+        return t1 && t2 && t3 && t4 && t5 && t6 && t7 && t8 && t9 && t10 && t11 && t12;
+    }
+
+    private void populateList(List list){
+        //populate list
+        for(int i = 0; i < 100; i++){
+            list.add(i);
+        }
+    }
+
+    public void clearList(List list){
+        while(!list.isEmpty()){
+            list.remove(0);
+        }
+    }
+
+    private boolean assertEquals(Object expected, Object attained, String test){
         boolean result = false;
         if(expected == null){
             result = expected == attained;
         }
         else{
             result = expected.equals(attained);
-        }
-        
+        }        
 
         String response = "Test ";
         response +=  result ? "Passed | " + test : "Failed | " + test + " : " + attained;
 
         System.out.println(response);
+
+        return result;
     }
 }
